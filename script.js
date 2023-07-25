c3f85f37089b8a7d4b42a10f887cd261d4995168
Zasada94 <pawelzasada2@wp.pl>
2023-07-25 14:36:15 +0200

    getting all info from db

--- script.js
+++ script.js
@@ -11,6 +11,34 @@ async function fetchData() {
 	}
 }
 
+function getMonth(pdd) {
+	if (pdd.slice(5, 7) === "01") {
+		return "styczeń ";
+	} else if (pdd.slice(5, 7) === "02") {
+		return "luty ";
+	} else if (pdd.slice(5, 7) === "03") {
+		return "marzec ";
+	} else if (pdd.slice(5, 7) === "04") {
+		return "kwiecień ";
+	} else if (pdd.slice(5, 7) === "05") {
+		return "maj ";
+	} else if (pdd.slice(5, 7) === "06") {
+		return "czerwiec ";
+	} else if (pdd.slice(5, 7) === "07") {
+		return "lipiec ";
+	} else if (pdd.slice(5, 7) === "08") {
+		return "sierpień ";
+	} else if (pdd.slice(5, 7) === "09") {
+		return "wrzesień ";
+	} else if (pdd.slice(5, 7) === "10") {
+		return "październik ";
+	} else if (pdd.slice(5, 7) === "11") {
+		return "listopad ";
+	} else if (pdd.slice(5, 7) === "12") {
+		return "grudzień ";
+	}
+}
+
 function renderVehicleList(vehicles) {
 	const vehicleList = document.getElementById("vehicleList");
 	vehicleList.innerHTML = "";
@@ -28,17 +56,41 @@ function renderVehicleList(vehicles) {
 		const subTitle = document.createElement("h3");
 		subTitle.textContent = vehicle.offer_details.kabina;
 
+		const availability = document.createElement("h3");
+		availability.textContent = !!vehicle.in_stock
+			? "Dostępny od ręki!"
+			: "Przewidywana data dostawy " +
+			  getMonth(vehicle.pdd) +
+			  vehicle.pdd.slice(0, 4);
+
+		const productionYear = document.createElement("p");
+		productionYear.textContent = "Rok produkcji: " + vehicle.pyear;
+
+		const gear = document.createElement("p");
+		gear.textContent =
+			"Skrzynia: " +
+			(vehicle.offer_details.skrzynia_automatyczna
+				? "Automatyczna"
+				: "Manualna");
+
 		const price = document.createElement("p");
-		price.textContent = "Cena: " + vehicle.car_price_disc + " zł netto";
+		price.textContent = "Cena netto: " + vehicle.car_price_disc + " zł";
+
+		const priceTotal = document.createElement("p");
+		priceTotal.textContent =
+			"Cena brutto: " + vehicle.total_gross_price + " zł";
 
 		const city = document.createElement("p");
 		city.textContent = "Miasto: " + vehicle.miasto;
-
-		card.appendChild(img);
 		card.appendChild(title);
 		card.appendChild(subTitle);
-		card.appendChild(price);
+		card.appendChild(availability);
+		card.appendChild(img);
+		card.appendChild(productionYear);
+		card.appendChild(gear);
 		card.appendChild(city);
+		card.appendChild(price);
+		card.appendChild(priceTotal);
 
 		vehicleList.appendChild(card);
 	});
@@ -46,5 +98,5 @@ function renderVehicleList(vehicles) {
 
 document.addEventListener("DOMContentLoaded", async () => {
 	const data = await fetchData();
-    renderVehicleList(data);
+	renderVehicleList(data);
 });
